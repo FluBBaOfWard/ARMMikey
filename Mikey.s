FluBBaOfWard/ARMMikey

commit 3a72a719e7aa1318f56edf4a4f381954c9b66463

Optimized register access.

Mikey.s
@@ -8,13 +8,7 @@
 
 #ifdef __arm__
 
-#ifdef GBA
-	#include "../Shared/gba_asm.h"
-#elif NDS
-	#include "../Shared/nds_asm.h"
-#endif
 #include "ARMMikey.i"
-#include "ARM6502/M6502.i"
 #include "../LynxCart/LynxCart.i"
 
 	.global mikeyInit
@@ -72,8 +66,6 @@ mikeyReset:				;@ r10=mikptr
 	ldr r0,=cart_0
 	str r0,[mikptr,#mikCartPtr]
 
-//	b miRegistersReset
-
 dummyFunc:
 	bx lr
 ;@----------------------------------------------------------------------------
@@ -99,11 +91,6 @@ memCopy:
 thumbCallR3:
 ;@----------------------------------------------------------------------------
 	bx r3
-;@----------------------------------------------------------------------------
-miRegistersReset:			;@ in r3=SOC
-;@----------------------------------------------------------------------------
-	ldrb r1,[mikptr,#mikLCDVSize]
-	b miRefW
 
 ;@----------------------------------------------------------------------------
 mikeySaveState:			;@ In r0=destination, r1=mikptr. Out r0=state size.
@@ -371,18 +358,17 @@ miUnmappedR:
 ;@----------------------------------------------------------------------------
 miUnknownR:
 ;@----------------------------------------------------------------------------
-	ldr r2,=0x826EBAD0
+	ldr r1,=0x826EBAD0
 ;@----------------------------------------------------------------------------
 miImportantR:
 	mov r11,r11					;@ No$GBA breakpoint
-	stmfd sp!,{r0,mikptr,lr}
+	stmfd sp!,{r2,mikptr,lr}
 	bl _debugIOUnimplR
-	ldmfd sp!,{r0,mikptr,lr}
+	ldmfd sp!,{r2,mikptr,lr}
 ;@----------------------------------------------------------------------------
 miRegR:
-	and r0,r0,#0xFF
-	add r2,mikptr,#mikRegs
-	ldrb r0,[r2,r0]
+	add r2,r2,#mikRegs
+	ldrb r0,[mikptr,r2]
 	bx lr
 	.pool
 
@@ -833,11 +819,9 @@ miUnknownW:
 ;@----------------------------------------------------------------------------
 miImportantW:
 ;@----------------------------------------------------------------------------
-	and r0,r0,#0xFF
-	add r2,mikptr,#mikRegs
-	strb r1,[r2,r0]
-	ldr r2,=debugIOUnimplW
-	bx r2
+	add r2,r2,#mikRegs
+	strb r1,[mikptr,r2]
+	b debugIOUnimplW
 ;@----------------------------------------------------------------------------
 miReadOnlyW:
 ;@----------------------------------------------------------------------------
@@ -846,9 +830,8 @@ miUnmappedW:
 	b _debugIOUnmappedW
 ;@----------------------------------------------------------------------------
 miRegW:
-	and r0,r0,#0xFF
-	add r2,mikptr,#mikRegs
-	strb r1,[r2,r0]
+	add r2,r2,#mikRegs
+	strb r1,[mikptr,r2]
 	bx lr
 
 ;@----------------------------------------------------------------------------
@@ -1382,10 +1365,8 @@ miRefW:						;@ 0x2001, Last scan line.
 
 ;@----------------------------------------------------------------------------
 mikSysUpdate:
-	.type	mikSysUpdate STT_FUNC
 ;@----------------------------------------------------------------------------
 	stmfd sp!,{r4-r11,lr}
-	ldr mikptr,=mikey_0
 	ldr r4,[mikptr,#systemCycleCount]
 	ldr r5,=159*105*16			;@ Cycle count for 60Hz frame.
 	add r5,r5,r4

ARMMikey.h
@@ -240,8 +240,6 @@ int mikeyLoadState(MIKEY *chip, const void *source);
  */
 int mikeyGetStateSize(void);
 
-void mikSysUpdate(void);
-
 #ifdef __cplusplus
 } // extern "C"
 #endif

commit e8afad55a6fc1fdd7e037d0c805568c81f18152d

Fixed TimXCntR, regression from rev:3a72a71. Fixed ComlynxRX/TX... regression from rev:0ab025a.

Mikey.s
@@ -384,11 +384,11 @@ miRegR:
 ;@----------------------------------------------------------------------------
 miTimXCntR:					;@ Timer X Count (0xFDX2/6/A/E)
 ;@----------------------------------------------------------------------------
-	stmfd sp!,{r0,r4,lr}
+	stmfd sp!,{r2,r4,lr}
 	ldr r4,[mikptr,#systemCycleCount]
 	bl mikUpdate
 	str r4,[mikptr,#systemCycleCount]
-	ldmfd sp!,{r0,r4,lr}
+	ldmfd sp!,{r2,r4,lr}
 	b miRegR
 
 ;@----------------------------------------------------------------------------
@@ -1575,9 +1575,9 @@ ComLynxRxData:				;@ In r0=MIKEY, r1=data
 	add r2,r2,#1
 	str r2,[r0,#uart_Rx_waiting]
 	;@ Receive the byte
-	add r2,mikptr,#uart_Rx_input_queue
 	ldr r3,[r0,#uart_Rx_input_ptr]
-	str r1,[r2,r3]
+	add r2,r0,#uart_Rx_input_queue
+	str r1,[r2,r3,lsl#2]
 	add r3,r3,#1
 	and r3,r3,#UART_MAX_RX_QUEUE-1
 	str r3,[r0,#uart_Rx_input_ptr]
@@ -1597,11 +1597,11 @@ ComLynxTxLoopback:			;@ In r0=MIKEY, r1=data
 	add r2,r2,#1
 	str r2,[r0,#uart_Rx_waiting]
 	;@ Receive the byte
-	add r2,mikptr,#uart_Rx_input_queue
 	ldr r3,[r0,#uart_Rx_output_ptr]
+	add r2,r0,#uart_Rx_input_queue
 	sub r3,r3,#1
 	and r3,r3,#UART_MAX_RX_QUEUE-1
-	str r1,[r2,r3]
+	str r1,[r2,r3,lsl#2]
 	str r3,[r0,#uart_Rx_output_ptr]
 	bx lr
 ;@----------------------------------------------------------------------------